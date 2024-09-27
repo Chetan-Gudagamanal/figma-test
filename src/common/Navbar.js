695052b7cbbbced66159ce13aed8fdd6c054d834
Chetan-Gudagamanal <chetanbg95@gmail.com>
2024-09-27 15:29:16 +0530

    added mui components and styles

--- src/common/Navbar.js
+++ src/common/Navbar.js
@@ -1,15 +1,56 @@
-// import * as styles from './navabar.module.css';
-import styles from "./navbarStyles.module.css"
+
 import take_off_logo from '../utils/icons/🦆 icon _ take off_.png';
 import fense_logo from '../utils/icons/🦆 icon _Fence_.png';
 import upload_logo from '../utils/icons/🦆 icon _upload_.png';
 
-export default function Navbar() {
-    return (
-        <div className={styles.Navbar}>
-            <img src={take_off_logo} className="App-logo" alt="logo" />
-            <img src={fense_logo} className="App-logo" alt="logo" />
-            <img src={upload_logo} className="App-logo" alt="logo" />
-        </div>
-    );
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
+import AdbIcon from '@mui/icons-material/Adb';
+
+const pages = ['Products', 'Pricing', 'Blog'];
+const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
+
+function ResponsiveAppBar() {
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
+  return (
+    <AppBar position="static">
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+        <img src={take_off_logo} className="App-logo" alt="logo" />
+        <img src={fense_logo} className="App-logo" alt="logo" />
+        <img src={upload_logo} className="App-logo" alt="logo" />
+        {/* <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} /> */}
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
 }
+export default ResponsiveAppBar;
